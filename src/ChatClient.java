c20cbe97bfbea0e504810e01f190f26c91d7a3d4
unknown <your email>
2018-09-03 22:21:20 +0900

    ログイン処理をメインに追加

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -8,10 +8,23 @@ class ChatClient {
     private MessageReceiver receiver;
 
     void start() {
-        connectServer("127.0.0.1", 10000);
-        openStream();
-        sender.start();
-        receiver.start();
+        System.out.println("ユーザーIDを入力してください");
+        int usId = new java.util.Scanner(System.in).nextInt();
+        System.out.println("パスワードを入力してください");
+        String usPs = new java.util.Scanner(System.in).nextLine();
+
+        boolean bl = LoginUtils.check(usId,usPs);
+
+        if(bl) {
+            System.out.println("正しくログインしました");
+            connectServer("127.0.0.1", 10000);
+            openStream();
+            sender.start();
+            receiver.start();
+        }
+        else{
+            System.out.println("ログインに失敗しました");
+        }
     }
 
     private void connectServer(String host, int port) {
