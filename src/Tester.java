10eddebff4600f7671aa1fda66767aaa63494da9
kazusa4418 <astarte.rododaktylos@gmail.com>
2018-09-09 21:28:35 +0900

    [fix] 色々修正

--- src/Tester.java
+++ src/Tester.java
@@ -8,14 +8,10 @@ public class Tester {
     private static BufferedWriter writer;
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1", 10000);
+        String s = " ";
 
-        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-    }
+        String[] ss = s.split(" ");
 
-    public void sendMessage(String msg) throws IOException {
-        writer.write(msg);
-        writer.newLine();
-        writer.flush();
+        System.out.println(ss.length);
     }
 }
