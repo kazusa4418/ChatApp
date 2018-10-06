f0bcf7687d99c39be62edaa214664a587beac383
finne4418 <kazusa4418@docomo.ne.jp>
2018-10-06 17:00:36 +0900

    [fix] fix

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ javac -d ../production/ChatApp/ event/*.java
 javac -d ../production/ChatApp/ main/*.java
 javac -d ../production/ChatApp/ mysql/*.java
 javac -d ../production/ChatApp/ server/*.java
-javac ../production/ChatApp/ util/*.java
+javac -d ../production/ChatApp/ util/*.java
